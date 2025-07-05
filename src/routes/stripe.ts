3e87d28bb86820c023d7f5f6159a15fbf3f48c3b
Daksh Kulshrestha <akshd@outlook.com>
2025-07-05 12:50:25 +0530

    feat: add credits and receipts

--- src/routes/stripe.ts
+++ src/routes/stripe.ts
@@ -2,6 +2,7 @@ import type Elysia from "elysia";
 import { env } from "../lib/env";
 import logger from "../lib/logger";
 import stripe from "../lib/stripe";
+import prisma from "../lib/prisma";
 
 export const stripeRoutes = (app: Elysia) =>
   app.group("/stripe", (app) =>
@@ -26,7 +27,35 @@ export const stripeRoutes = (app: Elysia) =>
           switch(event.type) {
             case "charge.succeeded":
               const session = event.data.object
-              console.log(session.metadata.address)
+              const charge = session.id
+              const amount = session.amount / 100
+              const user = await prisma.user.findUnique({
+                where: {
+                  address: session.metadata?.user
+                }
+              })
+              if (!user) {
+                logger.error("User not found")
+                return {
+                  message: "User not found"
+                }
+              }
+              await prisma.payment.create({
+                data: {
+                  user_id: user.id,
+                  amount: amount,
+                  stripe_id: charge,
+                  mode: session.payment_method || "",
+                  payment_id: charge,
+                  receipt_url: session.receipt_url || "",
+                }
+              })
+              await prisma.credits.create({
+                data: {
+                  user_id: user.id,
+                  credits: amount,
+                }
+              })
               break;
           }
         } catch (error) {
